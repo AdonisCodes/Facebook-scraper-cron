21881004c63246a92a2342ae5936ec077d3a79f5
AdonisCodes <s.f.businessacc@gmail.com>
2023-09-27 12:06:55 +0200

    Removed some placeholders

--- main.py
+++ main.py
@@ -276,7 +276,7 @@ def job():
     driver.quit()
     scrape_list(har, cookies, page_id)
 
-schedule.every().day.at("00:00").do(job)
+schedule.every().day.at("12:07").do(job)
 
 while True:
     schedule.run_pending()
